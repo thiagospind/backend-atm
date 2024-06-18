2fec1cb2f637827d7da2f0f376c99c126a92cbc9
thiago-yampa <thiago@yampa.com.br>
2024-06-18 00:43:46 -0300

    all

---
+++ src/main/java/br/com/spindola/atm/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,16 @@
+package br.com.spindola.atm.service.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends EntityNotFoundException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+}
